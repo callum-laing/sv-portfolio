46cc0e9478d25c205b11eeacc49381a79bad6af1
Callum Laing <callum.laing91@hotmail.co.uk>
2023-11-14 22:00:46 +0000

    added styling to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,16 +3,18 @@
 	<img src="hike-two.JPG" alt="" />
 	<span>
 		<img src="fleur.JPG" alt="" />
-		<p>
-			I'm a <span class="webdev-text">Front-End Web Developer</span> in England, UK, with a passion for
-			creating intuitive, dynamic and responsive user experiences.
-		</p>
+		<div class="p-text">
+			<p>
+				I'm a <span class="webdev-text">Front-End Web Developer</span> in England, UK, with a passion
+				for creating intuitive, dynamic and responsive user experiences.
+			</p>
 
-		<p>
-			While I'm deeply immersed in the world of web development, I also make sure to balance things
-			out by enjoying various sports, going on hikes, and spending quality time with my adorable
-			pup, Fleur.
-		</p>
+			<p>
+				While I'm deeply immersed in the world of web development, I also make sure to balance
+				things out by enjoying various sports, going on hikes, and spending quality time with my
+				adorable pup, Fleur.
+			</p>
+		</div>
 	</span>
 	<img src="hike-three.JPG" alt="" />
 </div>
@@ -24,6 +26,9 @@
 		font-family: arial;
 	}
 
+	.p-text {
+		margin-top: 70px;
+	}
 	h1 {
 		text-align: center;
 		font-size: 5rem;
@@ -45,17 +50,10 @@
 	}
 	img {
 		border-radius: 5px;
-		box-shadow: 2px 2px 2px 2px;
 		object-fit: contain;
 		width: 340px;
 		height: auto;
 		margin: 0 auto;
-	}
-
-	.wrap {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 50%;
+		box-shadow: #ff5733 0px 1px 4px;
 	}
 </style>
