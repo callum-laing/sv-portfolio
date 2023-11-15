70450b05d5431d808d7a3babc829e28deca00556
Callum Laing <callum.laing91@hotmail.co.uk>
2023-11-15 23:47:18 +0000

    amended styling

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -133,7 +133,7 @@
 
 	<div class="proj-two">
 		<div class="proj-text">
-			<h2>Etch-A-Sketch</h2>
+			<h2>Etch A Sketch</h2>
 			<p>
 				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
 				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
