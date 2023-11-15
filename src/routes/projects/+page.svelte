b9b979ec37a3c0e7eecd7fad42b80274e4d1df8d
Callum Laing <callum.laing91@hotmail.co.uk>
2023-11-15 00:22:49 +0000

    added buttons to project one

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -95,6 +95,12 @@
 				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
 				galley of type and scrambled it to make a type specimen book.
 			</p>
+			<button>
+				<a href="#">Github Repo</a>
+			</button>
+			<button>
+				<a href="#">Live Demo</a>
+			</button>
 		</div>
 		<img src="todo.JPG" alt="" />
 	</div>
