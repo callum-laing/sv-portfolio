8d45977968d73e8a467c6a60035c5a90dbf99d09
Callum Laing <callum.laing91@hotmail.co.uk>
2023-11-15 23:22:21 +0000

    added content and styling to project page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -88,44 +88,174 @@
 </div>
 <div class="projects-grid">
 	<div class="proj-one">
-		<div class="proj-one-text">
-			<h2>Dog Project</h2>
+		<div class="proj-text">
+			<h2><span>T</span>odo <span>L</span>ist</h2>
 			<p>
 				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
 				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
 				galley of type and scrambled it to make a type specimen book.
 			</p>
-			<button>
-				<a href="#">Github Repo</a>
-			</button>
-			<button>
-				<a href="#">Live Demo</a>
-			</button>
+			<div class="proj-btn">
+				<button>
+					<a href="#">Github Repo</a>
+				</button>
+				<button>
+					<a href="https://cdl-todo.netlify.app/">Live Demo</a>
+				</button>
+			</div>
 		</div>
-		<img src="todo.JPG" alt="" />
+		<img class="proj-img" src="todo.JPG" alt="" />
 	</div>
+
+	<div class="fade-line" />
+
+	<div class="proj-two">
+		<div class="proj-text">
+			<h2><span>C</span>alculator</h2>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+				galley of type and scrambled it to make a type specimen book.
+			</p>
+			<div class="proj-btn">
+				<button>
+					<a href="#">Github Repo</a>
+				</button>
+				<button>
+					<a href="https://callum-laing.github.io/calculator/">Live Demo</a>
+				</button>
+			</div>
+		</div>
+		<img class="proj-img" src="calculator.JPG" alt="" />
+	</div>
+
+	<div class="fade-line" />
+
+	<div class="proj-two">
+		<div class="proj-text">
+			<h2><span>E</span>tch-<span>A</span>-<span>S</span>ketch</h2>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+				galley of type and scrambled it to make a type specimen book.
+			</p>
+			<div class="proj-btn">
+				<button>
+					<a href="#">Github Repo</a>
+				</button>
+				<button>
+					<a href="https://callum-laing.github.io/etch-a-sketch/">Live Demo</a>
+				</button>
+			</div>
+		</div>
+		<img class="proj-img" src="etch.JPG" alt="" />
+	</div>
+
+	<footer class="footer-icons">
+		<div class="github">
+			<a href="https://github.com/callum-laing">
+				<svg
+					viewBox="0 0 20 20"
+					version="1.1"
+					xmlns="http://www.w3.org/2000/svg"
+					xmlns:xlink="http://www.w3.org/1999/xlink"
+					fill="#000000"
+					><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+						id="SVGRepo_tracerCarrier"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/><g id="SVGRepo_iconCarrier">
+						<title>github [#142]</title> <desc>Created with Sketch.</desc> <defs />
+						<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+							<g
+								id="Dribbble-Light-Preview"
+								transform="translate(-140.000000, -7559.000000)"
+								fill="#ff5733"
+							>
+								<g id="icons" transform="translate(56.000000, 160.000000)">
+									<path
+										d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
+										id="github-[#142]"
+									/>
+								</g>
+							</g>
+						</g>
+					</g></svg
+				></a
+			>
+		</div>
+		<div class="linkedin">
+			<a href="http://www.linkedin.com/in/callumlaing91">
+				<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+					><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+						id="SVGRepo_tracerCarrier"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/><g id="SVGRepo_iconCarrier">
+						<path
+							d="M6.5 8C7.32843 8 8 7.32843 8 6.5C8 5.67157 7.32843 5 6.5 5C5.67157 5 5 5.67157 5 6.5C5 7.32843 5.67157 8 6.5 8Z"
+							fill="#ff5733"
+						/>
+						<path
+							d="M5 10C5 9.44772 5.44772 9 6 9H7C7.55228 9 8 9.44771 8 10V18C8 18.5523 7.55228 19 7 19H6C5.44772 19 5 18.5523 5 18V10Z"
+							fill="#ff5733"
+						/>
+						<path
+							d="M11 19H12C12.5523 19 13 18.5523 13 18V13.5C13 12 16 11 16 13V18.0004C16 18.5527 16.4477 19 17 19H18C18.5523 19 19 18.5523 19 18V12C19 10 17.5 9 15.5 9C13.5 9 13 10.5 13 10.5V10C13 9.44771 12.5523 9 12 9H11C10.4477 9 10 9.44772 10 10V18C10 18.5523 10.4477 19 11 19Z"
+							fill="#ff5733"
+						/>
+						<path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M20 1C21.6569 1 23 2.34315 23 4V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H20ZM20 3C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H20Z"
+							fill="#ff5733"
+						/>
+					</g></svg
+				></a
+			>
+		</div>
+	</footer>
 </div>
 
 <style>
+	p,
+	a,
 	h1,
-	p {
+	h2,
+	i {
 		color: #ccd0e8;
+	}
+	p,
+	a {
 		font-family: verdana;
-		text-align: center;
+	}
+
+	h1,
+	h2 {
+		font-family: 'Fjalla One', sans-serif;
 	}
 
 	h1 {
 		font-size: 5rem;
 		margin-top: 20px;
-		font-family: 'Fjalla One', sans-serif;
 		letter-spacing: 5px;
+		text-align: center;
+	}
+
+	h2 {
+		letter-spacing: 5px;
+		font-size: 3rem;
+	}
+
+	span {
+		color: #ff5733;
 	}
 
 	.icon-wrapper {
 		display: flex;
 		gap: 20px;
 		justify-content: center;
-		text-align: center;
+		margin-bottom: 5%;
 	}
 
 	.html,
@@ -139,40 +269,78 @@
 	svg {
 		width: 50px;
 		height: auto;
-		fill: white;
+		fill: #ccd0e8;
 	}
 
-	i {
-		color: white;
+	.proj-one {
+		display: flex;
+		justify-content: center;
+		gap: 50px;
 	}
 
-	.proj-one > img {
-		width: 450px;
+	.proj-two {
+		display: flex;
+		justify-content: center;
+		gap: 50px;
+	}
+
+	.fade-line {
+		position: relative;
+		width: 100%;
+		height: 1px;
+		background: linear-gradient(to right, rgba(0, 0, 0, 0), #fff, rgba(0, 0, 0, 0));
+		margin-top: 200px;
+		margin-bottom: 200px;
+	}
+
+	.proj-img {
+		width: 400px;
 		height: auto;
 		border-radius: 10px;
 		box-shadow: #ff5733 0px 1px 4px;
 		opacity: 80%;
 	}
-	.proj-one > .proj-one-text {
-		width: 30%;
+	.proj-text {
+		max-width: 70ch;
 	}
 
-	.proj-one-text > h2 {
-		text-align: center;
-		color: white;
-		font-size: 2rem;
-	}
-	/* .proj-one {
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		place-items: center;
-	} */
-
-	.proj-one {
+	.proj-btn {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		gap: 10px;
-		margin-top: 150px;
+		gap: 50px;
+	}
+
+	button {
+		background-color: transparent;
+		border-radius: 10px;
+		padding: 10px;
+		box-shadow: #ff5733 0px 1px 4px;
+	}
+
+	button:hover {
+		background-color: #ff5733;
+	}
+
+	button > a {
+		text-decoration: none;
+	}
+
+	@media (max-width: 768px) {
+		img {
+			display: none;
+		}
+		h2 {
+			text-align: center;
+		}
+	}
+
+	.footer-icons {
+		border-top: 1px solid #ccd0e8;
+		margin-top: 100px;
+		padding: 20px;
+		display: flex;
+		gap: 20px;
+		justify-content: center;
 	}
 </style>
